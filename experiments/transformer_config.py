18b022025656d39a545499f7cd858147306f6a4c
TharinduDR <rhtdranasinghe@gmail.com>
2023-04-21 22:59:16 +0100

    001: Initial commit

--- experiments/transformer_config.py
+++ experiments/transformer_config.py
@@ -46,7 +46,7 @@ transformer_args = {
 
     'process_count': cpu_count() - 2 if cpu_count() > 2 else 1,
     'n_gpu': 1,
-    'use_multiprocessing': True,
+    'use_multiprocessing': False,
     "multiprocessing_chunksize": 500,
     'silent': False,
 
