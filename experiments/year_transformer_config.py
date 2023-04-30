b368fa9bf5bc3ee66ca42859a9b9bc8afd6aae2c
TharinduDR <rhtdranasinghe@gmail.com>
2023-04-30 15:18:46 +0100

    001: Initial commit

--- experiments/year_transformer_config.py
+++ experiments/year_transformer_config.py
@@ -1,13 +1,13 @@
 from multiprocessing import cpu_count
 
-TEMP_DIRECTORY = "temp_2019_LuxemBERT/"
+TEMP_DIRECTORY = "temp_2017_LuxemBERT/"
 SEED = 777
 
 
 transformer_args = {
-    'output_dir': 'temp_2019_LuxemBERT/outputs/',
-    "best_model_dir": "temp_2019_LuxemBERT/outputs/best_model",
-    'cache_dir': 'temp_2019_LuxemBERT/cache_dir/',
+    'output_dir': 'temp_2017_LuxemBERT/outputs/',
+    "best_model_dir": "temp_2017_LuxemBERT/outputs/best_model",
+    'cache_dir': 'temp_2017_LuxemBERT/cache_dir/',
 
     'fp16': False,
     'fp16_opt_level': 'O1',
