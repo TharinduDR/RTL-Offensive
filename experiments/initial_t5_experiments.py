7347398ebb96ffc8b7e4f4a54129237c45121388
TharinduDR <rhtdranasinghe@gmail.com>
2024-06-07 19:35:16 +0100

    001: Preparing demo

--- experiments/initial_t5_experiments.py
+++ experiments/initial_t5_experiments.py
@@ -8,10 +8,11 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
-
+from datasets import Dataset
+from datasets import load_dataset
+from config.model_args import T5Args
 from experiments.evaluation import sentence_label_evaluation, print_evaluation
 from experiments.label_converter import encode, decode
-from experiments.t5_config import t5_args, SEED
 from t5.t5_model import T5Model
 
 
@@ -27,39 +28,64 @@ MODEL_TYPE = arguments.model_type
 MODEL_NAME = arguments.model_name
 cuda_device = int(arguments.cuda_device)
 
-data_df = pd.read_json('data/rtl_comments_clean_2022-10.json')
-data_df_clean = data_df[data_df.status.isin(["Published", "Archived"])]
-data_df_clean['date_created'] = pd.to_datetime(data_df_clean['date_created'])
+model_args = T5Args()
+model_args.num_train_epochs = 10
+model_args.no_save = False
+model_args.fp16 = False
+model_args.learning_rate = 1e-4
+model_args.train_batch_size = 8
+model_args.max_seq_length = 256
+model_args.evaluate_generated_text = True
+model_args.evaluate_during_training = True
+model_args.evaluate_during_training_verbose = True
+model_args.evaluate_during_training_steps = 10000
+model_args.use_multiprocessing = False
+model_args.use_multiprocessing_for_evaluation = False
+model_args.use_multiprocessed_decoding = False
+model_args.overwrite_output_dir = True
+model_args.save_recent_only = True
+model_args.logging_steps = 10000
+model_args.manual_seed = 777
+model_args.early_stopping_patience = 25
+model_args.save_steps = 10000
 
-# Extract year and use 2022 data to create a test set.
-data_df_clean['year'] = data_df_clean['date_created'].dt.year
+t5_args = model_args
+n_fold = 1
 
-train = data_df_clean[(data_df_clean["year"] != 2022)]
-test = data_df_clean[(data_df_clean["year"] == 2022)]
+model_representation = MODEL_NAME.replace('/', '-')
+model_args.output_dir = os.path.join("outputs", model_representation)
+model_args.best_model_dir = os.path.join("outputs", model_representation, "best_model")
+model_args.cache_dir = os.path.join("cache_dir", model_representation)
 
-train = train.rename(columns={'text': 'input_text', 'status': 'target_text'})
+model_args.wandb_project = "LUX Comment Moderation"
+model_args.wandb_kwargs = {"name": MODEL_NAME}
+
+train = Dataset.to_pandas(load_dataset('instilux/lb-rtl-comments_clas', split='train'))
+test = Dataset.to_pandas(load_dataset('instilux/lb-rtl-comments_clas', split='test'))
+
+train = train.rename(columns={'text': 'input_text', 'label': 'target_text'})
 train = train[['input_text', 'target_text']]
 train["prefix"] = ""
 
-test = test.rename(columns={'status': 'labels'})
+test = test.rename(columns={'label': 'labels'})
 test = test[['text', 'labels']]
 
 test_sentences = []
 for index, row in test.iterrows():
     test_sentences.append("" + row['text'])
 
-test_preds = np.zeros((len(test_sentences), t5_args["n_fold"]))
+test_preds = np.zeros((len(test_sentences), n_fold))
 macro_f1_scores = []
 weighted_f1_scores = []
 
-for i in range(t5_args["n_fold"]):
-    if os.path.exists(t5_args['output_dir']) and os.path.isdir(t5_args['output_dir']):
-        shutil.rmtree(t5_args['output_dir'])
+for i in range(n_fold):
+    if os.path.exists(model_args.output_dir) and os.path.isdir(model_args.output_dir):
+        shutil.rmtree(model_args.output_dir)
 
     print("Started Fold {}".format(i))
     model = T5Model(MODEL_TYPE, MODEL_NAME, args=t5_args,
                                 use_cuda=torch.cuda.is_available())
-    train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED * i)
+    train_df, eval_df = train_test_split(train, test_size=0.1, random_state=model_args.manual_seed * i)
 
     model.train_model(train_df, eval_data=eval_df)
 
